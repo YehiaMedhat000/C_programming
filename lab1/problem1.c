f08a4ccce2c1b7d3eed01a233ff0a86d7578bf24
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-02-15 18:59:10 +0200

    lab1: Added first lab dir

---
+++ lab1/problem1.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+/* Macro for defining PI with limited precision */
+#define PI 3.141592653589
+
+
+/* Square Circumference */
+double square_circ(double a);
+/* Rectangle Circumference */
+double rec_circ(double a, double b);
+/* Circle Circumference */
+double circle_circ(double R);
+
+/**
+* main - Program to for problem 1
+* Return: Always (0)
+*/
+
+int main()
+{
+	double square_side;
+	double rec_a;
+	double rec_b;
+	double radius;
+
+	/* For square */
+	printf("Enter the side length of the square: ");
+	scanf("%lf", &square_side);
+	printf("The square circumference: %lf\n", square_circ(square_side));
+
+	/* For rectangle */
+	printf("Enter first side length of the rectangle: ");
+	scanf("%lf", &rec_a);
+	printf("Enter second side length of the rectangle: ");
+	scanf("%lf", &rec_b);
+	printf("The rectangle circumference: %lf\n", rec_circ(rec_a, rec_b));
+
+	/* For circle */
+	printf("Enter the radius of the circle: ");
+	scanf("%lf", &radius);
+	printf("The circle circumference: %lf\n", circle_circ(radius));
+
+	return (0);
+}
+
+/* Functions' definition */
+double square_circ(double a)
+{
+	return (a * 4);
+}
+
+double rec_circ(double a, double b)
+{
+	return ((a + b) * 2);
+}
+
+double circle_circ(double R)
+{
+	return (2 * PI * R);
+}
---
+++ lab1/sources.md
@@ -0,0 +1,6 @@
+# Sources for lab1 problems
+
+- [scanf Basics](https://www.youtube.com/watch?v=jr0hX1iMxrE) 
+- [Function Basics](https://www.youtube.com/watch?v=NGQoKF2Ggt8&pp=ygULZnVuY3Rpb25zLGM%3D)
+- [Short introduction to header files in C](https://www.youtube.com/watch?v=u1e0gLoz1SU&pp=ygUOaGVhZGVyLGZpbGVzLGM%3D)
+
