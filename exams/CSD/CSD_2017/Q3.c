e732862d92064b0f125f0c0c7abddf3a588868b5
YehiaMedhat000 <avhmedmvmedhat000@gmail.com>
2024-05-11 19:05:14 +0300

    Done some changes to the repo

---
+++ exams/CSD/CSD_2017/Q3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @ac: Arguments count
+ * @av: Arguments array
+ * Return: 0 in success, -1 otherwise
+ */
+
+int main(int ac, char **av)
+{
+	/* Place holder for now */
+	return (0);
+}
---
+++ quizzes/test.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+void get_matrix(int matrix[3][3]);
+void print_matrix(int matrix[3][3]);
+void multiply_matrix(int matrix1[3][3], int matrix2[3][3], int ret[3][3]);
+
+int main()
+{
+	int matrix[3][3];
+	int ret[3][3];
+	int unity[3][3] = {
+						{1, 1, 1},
+						{1, 1, 1},
+						{1, 1, 1}
+						};
+
+	get_matrix(matrix);
+	multiply_matrix(unity, matrix, ret);
+	print_matrix(ret);
+	return (0);
+}
+
+void get_matrix(int matrix[3][3])
+{
+	int i, j;
+
+	for (j = 0; j < 3; j++)
+	{
+		for (i = 0; i < 3; i++)
+			scanf("%d", &matrix[j][i]);
+	}
+}
+
+void print_matrix(int matrix[3][3])
+{
+	int i, j;
+
+	for (j = 0; j < 3; j++)
+	{
+		for (i = 0; i < 3; i++)
+			printf("%d ", matrix[j][i]);
+		printf("\n");
+	}
+}
+
+void multiply_matrix(int matrix1[3][3], int matrix2[3][3], int ret[3][3])
+{
+	int sum, i, j, k, col;
+
+	for (k = 0; k < 3; k++)
+	{
+		for (i = 0, col = 0; i < 3; i++)
+		{
+			sum = 0;
+			for (j = 0; j < 3; j++)
+				sum += matrix1[i][j] * matrix2[j][i];
+			/* Put the value */
+			ret[k][i] = sum;
+		}
+	}
+}
